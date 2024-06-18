bb2c7fcfe20a94aeb5f292d6423f6592b4e61f17
Andreja <andrejagit23@gmail.com>
2024-06-18 20:48:17 +0200

    Adding get stats for org route in router

--- routes/statsRoutes.js
+++ routes/statsRoutes.js
@@ -42,4 +42,42 @@ router.get('/api/stats/:month', (req, res) => {
     })
 })
 
+router.get('/api/stats/:month/:orgId', (req, res) => {
+
+    const yearMonth = req.params.month;
+    const orgId = req.params.orgId;
+    //console.log("yearMonth is: " + yearMonth);
+
+    fs.readFile(statsFilePath, 'utf-8', (err, data) => {
+        if (err){
+            return res.status(500).json({ error: 'Error reading file' });
+        }
+
+        if (data.trim().length == 0) {
+            //console.log("IT IS EMPTY");
+            return res.json("Empty");            
+        }
+
+        try {
+           // console.log("DATA IS: " + JSON.stringify(data));
+            const stats = JSON.parse(data);
+
+            const result = {};
+
+            for (const key in stats) {   
+                if (key == orgId) {
+                    result[key] = stats[key][yearMonth];
+                }             
+            }
+
+            res.json(result);
+
+        } catch (error) {
+            res.status(500).json({ error: 'Error parsing JSON 2323' });
+        }
+        
+
+    })
+})
+
 module.exports = router;

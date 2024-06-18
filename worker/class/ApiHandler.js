fd53ac66a2768e726330dee4a45be3f5d59205b3
Andreja <andrejagit23@gmail.com>
2024-06-18 15:57:17 +0200

    Implementing functions that get org details, project details etc via API

--- worker/class/ApiHandler.js
+++ worker/class/ApiHandler.js
@@ -19,7 +19,56 @@ class ApiHandler {
     
         } catch (error) {
             console.log("Error, please see: " + error);
+            return null;
         } 
+    }
+    
+    fetchOrgDetails = async (organizationId, sessionToken) => {
+        try {
+            const response = await axios.get(`https://www.insidemaps.com/api/v2/organizations/${organizationId}`, {
+                headers: {
+                    'Authorization': `Bearer ${sessionToken}`
+                }
+            });
+            return response.data.data;
+          
+        } catch (error) {
+            console.error(`Error fetching organization details`, error);
+            return null;
+        }
+    }
+    
+    fetchProjectIds = async (organizationId, sessionToken) => {
+        try {
+            const response = await axios.get(`https://stage.insidemaps.com/api/v2/organizations/${organizationId}/projects`, {
+                headers: {
+                    'Authorization': `Bearer ${sessionToken}`
+                }
+            });
+            return response.data.data;
+        } catch (error) {
+            console.error('Error fetching project IDs from API:', error);
+            return null;
+        }
+    }
+
+    async fetchProjectDetails(projectId, sessionToken) {
+        try {
+            const response = await axios.get(`https://stage.insidemaps.com/api/v2/projects/${projectId}`, {
+                headers: {
+                    'Authorization': `Bearer ${sessionToken}`
+                }
+            });
+            const projectData = response.data.data;
+            //console.log("Project data status is" + projectData.status);
+            //console.log("Project data PAID is" + projectData.paid);
+            return projectData; 
+        } catch (error) {
+            console.error(`Error fetching project details for project ${projectId}:`, error);
+            return null;
+        }
     }
 
 }
+
+module.exports = new ApiHandler();
